4081a9e6820455d5792cbee41f18ca4a1e75bacd
seovin <seobin930@gmail.com>
2022-03-23 11:54:28 +0900

    공지사항 css 추가

--- mall1/src/main/java/com/gura/spring/notice/service/NoticeServiceImpl.java
+++ mall1/src/main/java/com/gura/spring/notice/service/NoticeServiceImpl.java
@@ -31,7 +31,7 @@ public class NoticeServiceImpl implements NoticeService{
 	@Override
 	public void getList(HttpServletRequest request, HttpSession session) {
 		//한 페이지에 몇개씩 표시할 것인지
-		final int PAGE_ROW_COUNT=5;
+		final int PAGE_ROW_COUNT=10;
 		//하단 페이지를 몇개씩 표시할 것인지
 		final int PAGE_DISPLAY_COUNT=5;
 				
